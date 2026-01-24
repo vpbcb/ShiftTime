ac58ccbfd110c712ef4bcba990afa292e21651be
vpbcb <vpbcb1@gmail.com>
2026-01-24 14:30:36 +0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,18 @@
+﻿const CACHE = 'shift-calc-v1';
+const ASSETS = ['./', './index.html', './manifest.json'];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)));
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.map(k => (k === CACHE ? null : caches.delete(k))))
+    )
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(caches.match(event.request).then(resp => resp || fetch(event.request)));
+});
